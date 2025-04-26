16db5d39802be5f931eb9a257d748d815e72d3b6
Eren Önal <onaleren1@gmail.com>
2025-04-27 01:30:10 +0300

    update Google Analytics tracking ID in layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,14 +45,14 @@ export default function RootLayout({
       <head>
         <Script
           async
-          src="https://www.googletagmanager.com/gtag/js?id=G-F4QXSG320S"
+          src="https://www.googletagmanager.com/gtag/js?id=G-JPGXE80W8M"
         ></Script>
         <Script id="google-analytics">
           {` window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', 'G-F4QXSG320S');`}
+          gtag('config', 'G-JPGXE80W8M');`}
         </Script>
         <Analytics />
       </head>
